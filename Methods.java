795804bacb0a35faf6519496bc92b82d06a0edf6
caraywc <140350003+caraywc@users.noreply.github.com>
2024-03-26 23:01:45 -0500

    Add files via upload

---
+++ Methods.java
@@ -0,0 +1,39 @@
+public class Methods {
+
+//Method 1
+public static void main(String args[]) {
+    System.out.println("Hello World");
+}
+
+//Method 2
+public String myMethod() {
+    return "Something";
+}
+
+//Method 3
+public static int addNumbers(int x, int y) {
+    return x + y;
+ }
+
+//Method 4
+public double subtractNums(double x, double y, double z) {
+    return x + y + z;
+}
+
+//Method 5
+public static void printMessage(String message) {
+    System.out.println(message);
+}
+
+//Method 6
+public static int add() {
+    return 5 + 5;
+}
+
+//Method 7
+public static int value(short num1, short num2) {
+    int answer = num1 * num2;
+    return answer;
+}
+
+}
